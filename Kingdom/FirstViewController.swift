a396705aa75cc52c1510ab358e75b4c75baa5baa
Justin Haddadnia <haddadnia@haddadnia-macbookpro.roam.corp.google.com>
2020-04-16 19:03:05 -0400

    Initial Commit

---
+++ Kingdom/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Kingdom
+//
+//  Created by Justin Haddadnia on 4/16/20.
+//  Copyright © 2020 Justin Haddadnia. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Kingdom/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kingdom
+//
+//  Created by Justin Haddadnia on 4/16/20.
+//  Copyright © 2020 Justin Haddadnia. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
